9e8eee340f5c555e9aa77912c376f4f4b0ec0abb
yomna <yomnaothman@aucegypt.edu>
2024-06-17 04:19:47 +0300

    done with hero section

--- src/components/Here.jsx
+++ src/components/Here.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { logo } from '../assets';
 
-const Here = () => {
+const Hero = () => {
   return (
-    <div>Here</div>
+    <header classname="w-full flex justify-center items-center flex-col">
+        <nav classname="flex justify-between items-center w-full mb-10 pt-3">
+            <img src={logo} alt="digester logo" className='w-28 object-contain' />
+            <button 
+                type="button" 
+                onClick={() => window.open('https://github.com/yomnahisham/AI-ArticleDigest')}
+                className="black_btn">
+                GitHub
+            </button>
+        </nav>
+
+        <h1 className='head_text'>
+            Summarize Articles with <br className='max-md:hidden'/>
+            <span className='orange_gradient'> OpenAI GPT-4 </span>
+        </h1>
+
+        <h2 className='desc'>
+            Simplify your reading with Summize, an open-source article summarizer that transforms lengthy article summarizer that transforms length articles into clear and concise summaries
+        </h2>
+
+    </header>
   )
 }
 
-export default Here
+export default Hero
