005d7a6a76df61a2740b3e04a67c5a8aab977938
yomna <yomnaothman@aucegypt.edu>
2024-06-17 05:19:40 +0300

    started js

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+
+import App from './App.jsx';
+import { store } from './services/store';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
+    
   </React.StrictMode>,
 )
